831d6d42407f4ad14048845bad2d1458bc813ada
Sarah Panaligan <spianodev@gmail.com>
2023-01-13 10:58:02 -0600

    completed bank robbery

--- Q1 Exercises/01132023.py
+++ Q1 Exercises/01132023.py
@@ -1,4 +1,19 @@
+## Exercise 1 ##
 # It's your first Python code exercise! Starting out nice and easy :)
 # Use print()  to tell us your name!
 
 print('Sarah P')
+
+## Exercise 2 ##
+# Bank Robbery Money
+# I just robbed a bank with some of my friends.
+# We got away with $19,867,324,678,987.99, and now we have to split it up 5 ways.
+# Using the cash  variable I've already defined for you, print out the dollar amount each robber gets to keep.
+# (divide cash  by 5 and print the answer out)
+# p.s. I haven't actually robbed a bank.  Yet.
+
+cash = 19867324678987.99  # DON'T CHANGE THE CASH VARIABLE
+
+split = round(cash/5, 2)
+answer = "{:.2f}".format(split)
+print(answer)
