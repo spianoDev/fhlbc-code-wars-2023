06375e34e49107c496eac7cbbce0dbd3b3a7b212
Sarah Panaligan <spianodev@gmail.com>
2023-01-17 21:05:20 -0600

    wrote some variables

---
+++ Q1 Exercises/011723.py
@@ -0,0 +1,11 @@
+## Defining variables ##
+
+city = 'Chicago'
+price = 1000.01
+high_score = 4
+is_having_fun = True
+
+# bonus... use these variables to construct a sentence
+
+print(f'My current home is in {city}. The cost of anything breaking is ${price}. Today\'s winning number is '
+      f'{high_score} and it is {is_having_fun} that writing Python is fun!')
