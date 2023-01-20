4e84ad251cebb554a83455f31df4e028ed72db6a
Sarah Panaligan <spianodev@gmail.com>
2023-01-19 20:55:03 -0600

    fun with escapes

---
+++ Q1 Exercises/01192023.py
@@ -0,0 +1,13 @@
+# Set the message variable equal to any string containing a new-line escape sequence
+message = "Here is a message with a \n SURPRISE!"
+print(message)
+
+# Add a string to the mountains variable that when printed results in: /\/\/\
+# You will need to use an escape sequence more than once!
+mountains = "/\\/\\/\\"
+print(mountains)
+
+# Set the quotation variable to any string that contains an escaped double quotation mark
+
+quotation = "Quote from Twitter: \"Party City is filing for bankruptcy and will now be known as Party Pooper\""
+print(quotation)
