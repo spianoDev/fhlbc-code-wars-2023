73207ac9b8ba60cd3ce29f41154789ef82a7b5a6
Sarah Panaligan <spianodev@gmail.com>
2023-01-26 20:37:15 -0600

    added error handling

--- Q1 Exercises/01262023.py
+++ Q1 Exercises/01262023.py
@@ -11,25 +11,41 @@
 ## (This scenario requires a change of the radint values to extend beyond the given numbers)
 
 # NO TOUCHING==NO TOUCHING==NO TOUCHING==NO TOUCHING #| \
-from random import randint                           #|  \
-x = randint(-100, 100)                               #|   \
-while x == 0:  # make sure x isn't zero              #|    \
-    x = randint(-100, 100)                           #|     NO TOUCHING!!!!!! (please)
-y = randint(-100, 100)                               #|    /
-while y == 0:  # make sure y isn't zero              #|   /
-    y = randint(-100, 100)                           #|  /
-# NO TOUCHING==NO TOUCHING==NO TOUCHING==NO TOUCHING #| /
+from random import randint  # |  \
 
+x = randint(-120, 120)  # |   \
+while x == 0:  # make sure x isn't zero              #|    \
+    x = randint(-120, 120)  # |     NO TOUCHING!!!!!! (please)
+y = randint(-120, 120)  # |    /
+while y == 0:  # make sure y isn't zero              #|   /
+    y = randint(-120, 120)  # |  /
+# NO TOUCHING==NO TOUCHING==NO TOUCHING==NO TOUCHING #| /
 
 
 # Don't change the print statements so the tests can pass!
 # YOUR CODE GOES HERE vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 print(x, y)
+
+## Adding error handling for numbers beyond 100 or -100 first ##
+def too_high_or_low(num1, num2):
+    if num1 > 100 or num2 > 100:
+        print(f'ERROR, {num1} and/or {num2} is out of bounds!')
+        exit()
+    elif num1 < -100 or num2 < -100:
+        print(f'ERROR, {num1} and/or {num2} is out of bounds!')
+        exit()
+    else:
+        pass
+
+## Calling the function ##
+too_high_or_low(x, y)
+
+## If the error is not thrown, continue with the program ##
 if x > 0 and y > 0:
     print("both positive")
 elif x < 0 and y < 0:
     print("both negative")
-elif x > 0 and y < 0:
+elif x > 0 > y:
     print("x is positive and y is negative")
 else:
     print("y is positive and x is negative")
