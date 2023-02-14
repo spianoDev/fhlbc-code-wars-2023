f7f129fb3abce30abe062ab9f759462cc818e346
Sarah Panaligan <spianodev@gmail.com>
2023-02-13 20:42:01 -0600

    updated the code to wrap it inside a function

--- Q1 Exercises/rock_paper_scissors.py
+++ Q1 Exercises/rock_paper_scissors.py
@@ -1,4 +1,4 @@
-## Basic Version ##
+## Rock Paper Scissors Mini-Project ##
 print("...rock...")
 print("...paper...")
 print("...scissors...")
@@ -8,6 +8,7 @@ player2 = input("(enter Player 2's choice): ")
 
 print("SHOOT!")
 
+## Basic Version ##
 if player1 == player2:
     print('It\'s a DRAW!')
 elif player1.lower() == 'rock' and player2.lower() == 'scissors':
@@ -18,3 +19,26 @@ elif player1.lower() == 'paper' and player2.lower() == 'rock':
     print(f'Player 1 WINS with {player1}!')
 else:
     print(f'Player 2 WINS with {player2}!')
+
+## Refactor 1: wrap repeating elements inside a function ##
+
+
+def rock_paper_scissors(player1_choice, player2_choice):
+    winning_choices = {
+          'paper': 'rock',
+          'rock': 'scissors',
+          'scissors': 'paper'
+        }
+    if player1_choice.lower() == player2_choice.lower():
+        print('It\'s a DRAW!')
+    for choice in winning_choices.items():
+        if player1_choice.lower() == choice[0] and player2_choice.lower() == choice[1]:
+            print(f'Player 1 wins with {player1_choice}')
+            return player1_choice
+        elif player2_choice.lower() == choice[0] and player1_choice.lower() == choice[1]:
+            print(f'Player 2 wins with {player2_choice}')
+            return player2_choice
+
+
+rock_paper_scissors(player1, player2)
+
