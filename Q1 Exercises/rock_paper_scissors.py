19744c27dc69df54b05a0c49539be5cfbfb6954c
Sarah Panaligan <spianodev@gmail.com>
2023-02-16 18:23:37 -0600

    updated to have a random computer response

--- Q1 Exercises/rock_paper_scissors.py
+++ Q1 Exercises/rock_paper_scissors.py
@@ -4,7 +4,7 @@ print("...paper...")
 print("...scissors...")
 
 player1 = input("(enter Player 1's choice): ")
-player2 = input("(enter Player 2's choice): ")
+# player2 = input("(enter Player 2's choice): ")
 
 print("SHOOT!")
 
@@ -51,23 +51,47 @@ winning_choices = {
     'scissors': 'paper'
 }
 
+# def rock_paper_scissors(player1_choice, player2_choice):
+#     if player1_choice.lower() not in winning_choices:
+#         sys.exit(f'Player 1 entered an incorrect response of {player1_choice}. Please try again')
+#     elif player2_choice.lower() not in winning_choices:
+#         sys.exit(f'Player 2 entered an incorrect response of {player2_choice}. Please try again')
+#     else:
+#         for choice in winning_choices.items():
+#             if player1_choice.lower() == player2_choice.lower():
+#                 print('It\'s a DRAW!')
+#                 return
+#             elif player1_choice.lower() == choice[0] and player2_choice.lower() == choice[1]:
+#                 print(f'Player 1 wins with {player1_choice}')
+#                 return player1_choice
+#             elif player2_choice.lower() == choice[0] and player1_choice.lower() == choice[1]:
+#                 print(f'Player 2 wins with {player2_choice}')
+#                 return player2_choice
 
-def rock_paper_scissors(player1_choice, player2_choice):
+# rock_paper_scissors(player1, player2)
+from random import randint
+
+random_num = randint(0, 2)
+print(random_num)
+winners = list(winning_choices)
+computer = winners[random_num]
+print(computer)
+
+
+def rock_paper_scissors(player1_choice, computer_choice):
     if player1_choice.lower() not in winning_choices:
         sys.exit(f'Player 1 entered an incorrect response of {player1_choice}. Please try again')
-    elif player2_choice.lower() not in winning_choices:
-        sys.exit(f'Player 2 entered an incorrect response of {player2_choice}. Please try again')
     else:
         for choice in winning_choices.items():
-            if player1_choice.lower() == player2_choice.lower():
+            if player1_choice.lower() == computer_choice:
                 print('It\'s a DRAW!')
                 return
-            elif player1_choice.lower() == choice[0] and player2_choice.lower() == choice[1]:
-                print(f'Player 1 wins with {player1_choice}')
+            elif player1_choice.lower() == choice[0] and computer_choice == choice[1]:
+                print(f'Player 1 wins with {player1_choice} over {computer_choice}')
                 return player1_choice
-            elif player2_choice.lower() == choice[0] and player1_choice.lower() == choice[1]:
-                print(f'Player 2 wins with {player2_choice}')
-                return player2_choice
+            elif computer_choice == choice[0] and player1_choice.lower() == choice[1]:
+                print(f'Computer wins with {computer_choice} over {player1_choice}')
+                return computer_choice
 
-rock_paper_scissors(player1, player2)
 
+rock_paper_scissors(player1, computer)
