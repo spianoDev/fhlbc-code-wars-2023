c5fe64e3bed237bd8fd9bffbdad10133d50f7f8b
Sarah Panaligan <spianodev@gmail.com>
2023-02-28 20:42:01 -0600

    number labels

--- Q1 Exercises/02282023.py
+++ Q1 Exercises/02282023.py
@@ -10,3 +10,14 @@ while i < int(how_many):
     print('CLEAN UP YOUR ROOM!!')
     i += 1
 
+# Use loops, ranges, and conditionals to find unlucky numbers 1-20 inclusive
+# Numbers 4 & 13 are unlucky
+# Even numbers are even, odd numbers are odd
+
+for num in range(1, 21):
+    if num == 4 or num == 13:
+        print(f'{num} is UnLuCkY')
+    elif num % 2 == 0:
+        print(f'{num} is EVEN')
+    else:
+        print(f'{num} is odD')
