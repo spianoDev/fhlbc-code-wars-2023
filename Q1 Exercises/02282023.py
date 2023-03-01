93261a24c8a5fa8ee958f36a6d61424c275835a6
Sarah Panaligan <spianodev@gmail.com>
2023-02-28 20:55:46 -0600

    for and while emojis

--- Q1 Exercises/02282023.py
+++ Q1 Exercises/02282023.py
@@ -21,3 +21,14 @@ for num in range(1, 21):
         print(f'{num} is EVEN')
     else:
         print(f'{num} is odD')
+
+# Emoji Art
+# print emojis using both a for and while loop
+
+emojis = ['\U0001f929', '\U0001f911', '\U0001fae3', '\U0001f914', '\U0001f60f', '\U0001f644', '\U0001f635',
+          '\U0001f973', '\U0001f9d0', '\U0001f47b']
+i = 1
+for emoji in emojis:
+    while len(emojis) >= i:
+        print(emojis[i-1] * i)
+        i += 1
