c48b769cf939ef9d3f96deb8f802a1083b5f85b3
Sarah Panaligan <spianodev@gmail.com>
2023-02-28 20:36:33 -0600

    room cleaning

---
+++ Q1 Exercises/02282023.py
@@ -0,0 +1,12 @@
+## More Loop Exercises ##
+
+# Have a user imput a number.
+# Use a loop to print a string the number of time inputted.
+
+how_many = input('How many times do I have to tell you? ')
+
+i = 0
+while i < int(how_many):
+    print('CLEAN UP YOUR ROOM!!')
+    i += 1
+
