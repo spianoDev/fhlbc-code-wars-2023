91e53c5f458cc7a2075e86163fa54a354510a3a3
Sarah Panaligan <spianodev@gmail.com>
2023-04-24 20:31:02 -0500

    finished list comprehension exercises

--- Q2 Exercises/list_comprehension.py
+++ Q2 Exercises/list_comprehension.py
@@ -1,4 +1,4 @@
-# Exercise
+# Exercise 19: List Comprehension Exercises
 #
 og_lst = ['Elie', 'Tim', 'Matt']
 # Create a variable called answer that has the first letter of each name in the og_lst
@@ -9,3 +9,18 @@ num_lst = [1, 2, 3, 4, 5, 6]
 
 answer2 = [x for x in num_lst if x % 2 == 0]
 print(answer2)
+
+# Exercise 20: More List Comprehension
+
+lst1 = [1, 2, 3, 4]
+lst2 = [3, 4, 5, 6]
+# Find all the elements that overlap between the two lists
+
+combo_lst = []
+[(combo_lst.append(num)) for num in lst1 if num in lst2 and num not in combo_lst]
+print(combo_lst)
+
+words = ['Elie', 'Tim', 'Matt']
+# Reverse each word (using slice) and make all the character values lower case
+reversed_words = [name[::-1].lower() for name in words]
+print(reversed_words)
