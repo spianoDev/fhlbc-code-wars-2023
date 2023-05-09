40af00bf03e701beccf675920bacf643e316e876
Sarah Panaligan <spianodev@gmail.com>
2023-05-08 20:26:41 -0500

    completed bakery stock with print statements

--- Q2 Exercises/dictionary.py
+++ Q2 Exercises/dictionary.py
@@ -30,3 +30,27 @@ for value in donations.items():
     # print(value[1])
     total_donations += value[1]
 print(total_donations)
+
+# Exercise 28 Dictionary Access
+# This code picks a random food item:
+from random import choice
+food = choice(['cheese pizza', 'quiche', 'morning bun', 'gummy bear', 'tea cake'])
+
+bakery_stock = {
+    'almond croissant': 12,
+    'toffee cookie': 3,
+    'morning bun': 1,
+    'chocolate chunk cookie': 9,
+    'tea cake': 25
+}
+
+# Exercise is to print out the inventory if it exists in the bakery stock
+
+print(food)
+if food in bakery_stock.keys():
+    if bakery_stock[food] > 1:
+        print(f'There are {bakery_stock[food]} {food}s left!')
+    else:
+        print(f'There is {bakery_stock[food]} {food} left!')
+else:
+    print('We don\'t make that!')
