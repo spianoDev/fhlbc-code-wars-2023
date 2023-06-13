eb98b8e3042c81ba6a5bd30ec063278326829fb7
Sarah Panaligan <spianodev@gmail.com>
2023-06-12 21:06:35 -0500

    created dictionary methods

--- Q2 Exercises/dictionary_methods.py
+++ Q2 Exercises/dictionary_methods.py
@@ -17,3 +17,30 @@ stock_list.update({'cookie': 18})
 print(stock_list)
 stock_list.pop('cake')
 print(stock_list)
+
+# Exercise 31 #
+# Given two lists ["CA", "NJ", "RI"] and ["California", "New Jersey", "Rhode Island"]
+# create a dictionary that looks like this {'CA': 'California', 'NJ': 'New Jersey', 'RI': 'Rhode Island'}.
+# Save it to a variable called answer.
+
+list1 = ["CA", "NJ", "RI"]
+list2 = ["California", "New Jersey", "Rhode Island"]
+
+answer = dict(zip(list1, list2))
+print(answer)
+
+# Exercise 32 #
+# person = [["name", "Jared"], ["job", "Musician"], ["city", "Bern"]]
+#
+# Create a dictionary called person_info , that makes each first item in each list a key and the second item a
+# corresponding value.
+# {'name': 'Jared', 'job': 'Musician', 'city': 'Bern'}
+person = [["name", "Jared"], ["job", "Musician"], ["city", "Bern"]]
+
+# use the person variable in your answer
+person_info = {}
+
+for info in person:
+    person_info.update({info[0]: info[1]})
+
+print(person_info)
