4598ecbe29ab17f0615ba0cdf5159a49bef24111
Sarah Panaligan <spianodev@gmail.com>
2023-09-19 21:33:08 -0500

    completed product

---
+++ Q3 Exercises/more_functions.py
@@ -0,0 +1,15 @@
+# Exercise 42
+
+'''
+Write a function that returns the product of two parameters
+'''
+
+def multi(num1, num2):
+    print(num1*num2)
+    return num1 * num2
+
+
+multi(2, 2)
+multi(2, -2)
+
+
